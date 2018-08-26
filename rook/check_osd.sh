982d42e787893719d92406d2c40cb9f17edd4248
danch <danchrist100@gmail.com>
2018-08-26 18:24:56 +0000

    rook/ceph functional, as long as you feed it whole drives and modprobe rbd manually on centos 7.5

---
+++ rook/check_osd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get OSD Pods
+# This uses the example/default cluster name "rook"
+#OSD_PODS=$(kubectl get pods --all-namespaces -l \
+#  app=rook-ceph-osd,rook_cluster=rook -o jsonpath='{.items[*].metadata.name}')
+OSD_PODS="rook-ceph-osd-congratulations-zlxfx rook-ceph-osd-opti-jfcv6"
+
+# Find node and drive associations from OSD pods
+for pod in $(echo ${OSD_PODS})
+do
+ echo "Pod:  ${pod}"
+ echo "Node: $(kubectl -n rook get pod ${pod} -o jsonpath='{.spec.nodeName}')"
+ kubectl -n rook exec ${pod} -- sh -c '\
+  for i in /var/lib/rook/osd*; do
+    [ -f ${i}/ready ] || continue
+    echo -ne "-$(basename ${i}) "
+    echo $(lsblk -n -o NAME,SIZE ${i}/block 2> /dev/null || \
+    findmnt -n -v -o SOURCE,SIZE -T ${i}) $(cat ${i}/type)
+  done|sort -V
+  echo'
+done
